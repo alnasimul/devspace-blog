25b8ceb4d9925b6c2a1b4827301568c23f0640f0
Nasimul Haque <alnasimul@gmail.com>
2021-09-27 00:50:23 +0600

    bug-fixed

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -11,12 +11,12 @@ const PostPage = ({frontmatter, content, slug}) => {
     return (
         <Layout title={title}>
             <Link href='/blog'>
-                <a className='text-gray-900 hover:text-blue-600'>
+                <a className='text-gray-900 hover:text-blue-600 ml-9 md:mx-0'>
                     Go Back
                 </a></Link>
             <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
                 <div className="flex justify-between items-center mt-4">
-                    <h1 className="text-5xl mb-7">{title}</h1>
+                    <h1 className="text-xl font-bold mb-7 md:text-5xl">{title}</h1>
                     <CategoryLabel>{category}</CategoryLabel>
                 </div>
                 <img src={cover_image} alt="" className='w-full rounded'/>
